dadad8abc83ab623dcb6e73e083fca35f1cc7d3f
kalyanreddy75694-sudo <kalyanreddy75694@gmail.com>
2026-07-16 11:01:13 +0530

    Update Day 1-1295.Find number with even Number of Digits.java

--- Day 1-1295.Find number with even Number of Digits.java
+++ Day 1-1295.Find number with even Number of Digits.java
@@ -2,11 +2,7 @@ class Solution {
     public int findNumbers(int[] nums) {
         int count = 0;
         for (int num : nums) {
-            // Under the constraint 1 <= num <= 10^5,
-            // A number has an even number of digits if:
-            // - It has 2 digits: [10, 99]
-            // - It has 4 digits: [1000, 9999]
-            // - It has 6 digits: [100000] (boundary of 10^5)
+           
             if ((num >= 10 && num <= 99) || 
                 (num >= 1000 && num <= 9999) || 
                 num == 100000) {
