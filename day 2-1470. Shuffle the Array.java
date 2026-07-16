7645cbb2035d33b1c9b249e360d922e9b534c1f1
Tapala kalyan Readdy <kalyanreddy75694@gmail.com>
2026-07-16 13:15:39 +0530

    Update day 2-1470. Shuffle the Array.java

--- day 2-1470. Shuffle the Array.java
+++ day 2-1470. Shuffle the Array.java
@@ -2,7 +2,7 @@ class Solution {
     public int[] shuffle(int[] nums, int n) {
         int[] result = new int[2 * n];
         for (int i = 0; i < n; i++) {
-            result[2 * i] = nums[i];         // Places x_i
+         result[2 * i] = nums[i];         // Places x_i
             result[2 * i + 1] = nums[i + n]; // Places y_i
         }
         return result;
