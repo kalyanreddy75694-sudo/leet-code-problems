cc419c975f50114b66620f68d67413e91201cd9b
kalyanreddy75694-sudo <kalyanreddy75694@gmail.com>
2026-07-16 11:01:41 +0530

    Update day 1- 7.Reverse integer.java

--- day 1- 7.Reverse integer.java
+++ day 1- 7.Reverse integer.java
@@ -5,9 +5,9 @@ class Solution {
             int pop = x % 10;
             x /= 10;
             
-            // Check for overflow
+           
             if (rev > Integer.MAX_VALUE / 10 || (rev == Integer.MAX_VALUE / 10 && pop > 7)) return 0;
-            // Check for underflow
+           
             if (rev < Integer.MIN_VALUE / 10 || (rev == Integer.MIN_VALUE / 10 && pop < -8)) return 0;
             
             rev = rev * 10 + pop;
