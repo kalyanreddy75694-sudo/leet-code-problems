737430748cdd20d08d97be962be30278e42098a1
kalyanreddy75694-sudo <kalyanreddy75694@gmail.com>
2026-07-16 11:02:18 +0530

    Update day1 - 9.palindrome number.java

--- day1 - 9.palindrome number.java
+++ day1 - 9.palindrome number.java
@@ -9,9 +9,6 @@ class Solution {
             reversedHalf = reversedHalf * 10 + x % 10;
             x /= 10;
         }
-
-        // For even-length numbers, x should equal reversedHalf (e.g., 1221 -> x = 12, reversedHalf = 12)
-        // For odd-length numbers, we get rid of the middle digit by reversedHalf / 10 (e.g., 12321 -> x = 12, reversedHalf = 123)
         return x == reversedHalf || x == reversedHalf / 10;
     }
 }
