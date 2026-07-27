22d85d02001dbc251cb6096d3a03800aa1a14c55
Tapala kalyan Readdy <kalyanreddy75694@gmail.com>
2026-07-27 13:58:30 +0530

    Create 496.Next greater Element.java

---
+++ 496.Next greater Element.java
@@ -0,0 +1,28 @@
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        
+        Map<Integer, Integer> nextGreaterMap = new HashMap<>();
+        Deque<Integer> stack = new ArrayDeque<>();
+
+
+        for (int num : nums2) {
+            while (!stack.isEmpty() && stack.peek() < num) {
+                nextGreaterMap.put(stack.pop(), num);
+            }
+            stack.push(num);
+        }
+
+
+        int[] result = new int[nums1.length];
+        for (int i = 0; i < nums1.length; i++) {
+            result[i] = nextGreaterMap.getOrDefault(nums1[i], -1);
+        }
+
+        return result;
+    }
+}
