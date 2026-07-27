33dabebead1914d065bc99a2e609b013ecc56cca
Tapala kalyan Readdy <kalyanreddy75694@gmail.com>
2026-07-27 13:29:58 +0530

    Create 933.Number of Recent Calls.java

---
+++ 933.Number of Recent Calls.java
@@ -0,0 +1,24 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+class RecentCounter {
+    private Queue<Integer> requests;
+
+    public RecentCounter() {
+        requests = new LinkedList<>();
+    }
+    
+    public int ping(int t) {
+
+        requests.add(t);
+        
+
+        while (!requests.isEmpty() && requests.peek() < t - 3000) {
+            requests.poll();
+        }
+        
+        
+        return requests.size();
+    }
+}
+
